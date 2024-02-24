f6f401b49e6a53d1cbd85baea773f547bd46e3d4
Georg Schramm <georg.schramm@kuleuven.be>
2024-02-24 17:41:00 +0100

    cosmetics

--- myscript.py
+++ myscript.py
@@ -1,12 +1,18 @@
-# # A python script conveted to notebook / book
+# %% [markdown]
+# # My Script
 
-#+
+# %%
 import numpy as np
-x = np.linspace(-1,1,100)
+
+x = np.linspace(-1, 1, 100)
 y = x**3
 
-#+
-import matplotlib.pyplot as plt
-fig, ax = plt.subplots()
-ax.plot(x,y,'.-')
+# %% [markdown]
+# ## Let's plot the data
+# Because plotting is fun
 
+# %%
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots()
+ax.plot(x, y, ".-")
